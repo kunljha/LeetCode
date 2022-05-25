2a7bfd42ca391b0ff9966e986f8531f0132e4346
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-05-25 22:35:42 +0530

    Time: 31 ms (7.05%), Space: 15.8 MB (82.64%) - LeetHub

---
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        unordered_map<int, int> table;
+        
+        for(int &x: nums) {
+            if(table.find(x) != table.end())
+                table[x]++;
+            else
+                table.insert({x, 1});
+        }
+        
+        for(auto it = table.begin(); it != table.end(); it++) {
+            if(it->second > n/3) ans.push_back(it->first);
+        }
+        
+        return ans;
+    }
+};
