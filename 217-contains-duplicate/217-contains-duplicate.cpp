31b35538dd9629af4b36c70cd026cb02cba28583
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-05-30 23:33:44 +0530

    Time: 165 ms (19.07%), Space: 51.5 MB (51.61%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n = nums.size();
         unordered_map<int, bool> table;
         
         for(int &x: nums) {
