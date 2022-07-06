a6af8097b9090275b6530a7cdc0abdcaf9a6551b
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-07-06 23:14:17 +0530

    Time: 23 ms (14.48%), Space: 5.9 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,11 +3,6 @@ public:
     int fib(int n) {
         if(n == 0 || n == 1) return n;
         
-        int ans = 0, fib0 = 0, fib1 = 1;
-        for(int i = 0; i < n-1; i++) 
-           ans =  fib0 + fib1,
-           fib0 = fib1, fib1 = ans;
-        
-        return ans;  
+        return fib(n-1) + fib(n-2);  
     }
 };
