480de2543f71ce3f847a0ded410112959aa17893
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-07-06 23:25:06 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
+        vector<int> dp(n+1, -1);
+        
+        return fibonacci(n, dp);
+    }
+    
+    int fibonacci(int n, vector<int> &dp) {
         if(n == 0 || n == 1) return n;
         
-        return fib(n-1) + fib(n-2);  
+        if(dp[n] != -1) return dp[n];
+        
+        return dp[n] = fibonacci(n-1, dp) + fibonacci(n-2, dp);
     }
 };
