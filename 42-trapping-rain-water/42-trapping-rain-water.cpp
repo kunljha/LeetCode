44a2c2b724b450f8307fd0d33d77413ecca8ce21
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-07-13 13:02:29 +0530

    Time: 7 ms (99.64%), Space: 20.5 MB (32.50%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& arr) {
+        int n = arr.size();
+        
         int ans = 0;
-        int maxleft = 0, maxright = 0;
         
-        int l = 0, r = arr.size() - 1;
+        // prefix array to store max on left of arr[i]
+        vector<int> prefix(n, 0);
+        prefix[0] = arr[0];
         
-        while(l <= r) {
-            if(arr[l] <= arr[r]) {
-                if(arr[l] > maxleft)
-                    maxleft = arr[l];
-                else 
-                    ans += maxleft - arr[l];
-                
-                ++l;
-            } else {
-                if(arr[r] > maxright) 
-                    maxright = arr[r];
-                else 
-                    ans += maxright - arr[r];
-                
-                --r;
-            }
-        }
+        for(int i = 1; i < n; i++)
+            prefix[i] = max(arr[i], prefix[i-1]);
+        
+        // suffix array to store max on right of arr[i]
+        vector<int> suffix(n, 0);
+        suffix[n-1] = arr[n-1];
+        
+        for(int i = n-2; i >= 0; i--)
+            suffix[i] = max(arr[i], suffix[i+1]);
+        
+        // units of water trapped
+        for(int i = 0; i < n; i++) 
+            ans += min(prefix[i], suffix[i]) - arr[i];
         
         return ans;
     }
