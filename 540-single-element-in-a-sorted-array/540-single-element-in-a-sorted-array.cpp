287d781fda00cfdd7ebc4a7225deab8c021b234f
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-07-27 22:17:20 +0530

    Time: 47 ms (34.92%), Space: 22.3 MB (55.04%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -4,19 +4,12 @@ public:
         int low = 0, high = nums.size()-2;
         
         while(low <= high) {
-            int mid = (low + high)/2;
+            int mid = (low + high)>>1;
             
-            if(mid % 2 == 0) {
-                if(nums[mid] == nums[mid+1]) 
-                    low = mid+1;
-                else 
-                    high = mid-1;
-            } else {
-                if(nums[mid-1] == nums[mid])
-                    low = mid+1;
-                else 
-                    high = mid-1;
-            } 
+            if(nums[mid] == nums[mid^1]) 
+                low = mid+1;
+            else 
+                high = mid-1; 
         }
         
         return nums[low];
