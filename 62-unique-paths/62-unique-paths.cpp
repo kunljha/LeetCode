805913cfe298b4d34d572ff606ab6bccf39ddc48
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-08-01 21:13:42 +0530

    Time: 3 ms (44.81%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -3,16 +3,18 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, -1));
         
-        return unique(m, n, 0, 0, dp);
+        return paths(0, 0, m, n, dp);   
     }
     
-    int unique(int m, int n, int i, int j, vector<vector<int>> &dp) {
-        if(i == m || j == n) return 0; // out of bound
+    int paths(int i, int j, int m, int n, vector<vector<int>> &dp) {
+        if(i == m-1 && j == n-1) return 1;
         
-        if(i == m-1 && j == n-1) return 1; // reached destination
+        if(i == m || j == n) return 0;
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        return dp[i][j] = unique(m, n, i+1, j, dp) + unique(m, n, i, j+1, dp);
+        dp[i][j] = paths(i+1, j, m, n, dp) + paths(i, j+1, m, n, dp);
+        
+        return dp[i][j];
     }
 };
