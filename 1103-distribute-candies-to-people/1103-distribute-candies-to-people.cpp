b77720b090db0dd89edce89520a03e5a68903244
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-08-08 10:05:37 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (86.52%) - LeetHub

--- 1103-distribute-candies-to-people/1103-distribute-candies-to-people.cpp
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.cpp
@@ -3,9 +3,17 @@ public:
     vector<int> distributeCandies(int candies, int num_people) {
         vector<int> ans(num_people, 0);
         
-        for (int i = 0; candies > 0; ++i) {
-            ans[i % num_people] += min(candies, i + 1);
-            candies -= i + 1;
+        int last_given = 0;
+        
+        for(int i = 0; candies > 0; i = (i+1) % num_people) {
+            if(candies <= last_given+1) {
+                ans[i] += candies;
+                break;
+            } else {
+                ans[i] += last_given + 1;
+                ++last_given;
+                candies -= last_given;
+            }
         }
         
         return ans;
