d4bdf10808f0fcdf5596da9a5c8541cdfca20772
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-11-08 20:50:59 +0530

    Time: 3 ms (35.36%), Space: 5.7 MB (97.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     int climbStairs(int n) { 
-        vector<int> dp(n+1, 0);
-        
-        dp[0] = 1;
+        int prev2 = 0, prev = 1;
         
         for(int step = 1; step <= n; ++step) {
-            int oneStep = dp[step-1];
+            int oneStep = prev;
             int twoStep = 0;
             if(step > 1) 
-                twoStep = dp[step-2];
+                twoStep = prev2;
 
-            dp[step] = oneStep + twoStep;
+            int cur = oneStep + twoStep;
+            
+            prev2 = prev;
+            prev = cur;
         }
         
-        return dp[n];
+        return prev;
     }
-    
-//     int fun(int step, vector<int> &dp) {
-//         if(step == 0) return 1;
-        
-//         if(dp[step] != -1) return dp[step];
-        
-//         int oneStep = fun(step-1, dp);
-//         int twoStep = 0;
-//         if(step > 1) 
-//             twoStep = fun(step-2, dp);
-        
-//         return dp[step] = oneStep + twoStep;
-//     }
 };
