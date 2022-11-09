e4b7f596e66d11d203a1ddd0a9d92d6672068e04
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-11-09 12:14:24 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (25.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,20 +3,31 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         
-        vector<int> dp(n, -1);
+        vector<int> dp(n, 0);
+        
+        dp[0] = nums[0];
+        
+        for(int ind = 1; ind < n; ++ind) {
+            int notRobbed = 0 + dp[ind-1]; 
+            
+            int robbed = nums[ind];
+            if(ind > 1) robbed += dp[ind-2];
 
-        return fun(n-1, nums, dp);
+            dp[ind] = max(robbed, notRobbed);
+        }
+
+        return dp[n-1];
     }
     
-    int fun(int ind, vector<int> &nums, vector<int> &dp) {
-        if(ind < 0) return 0;
-        if(ind == 0) return nums[0];
+//     int fun(int ind, vector<int> &nums, vector<int> &dp) {
+//         if(ind < 0) return 0;
+//         if(ind == 0) return nums[0];
         
-        if(dp[ind] != -1) return dp[ind];
+//         if(dp[ind] != -1) return dp[ind];
         
-        int robbed = nums[ind] + fun(ind-2, nums, dp);
-        int notRobbed = 0 + fun(ind-1, nums, dp);
+//         int robbed = nums[ind] + fun(ind-2, nums, dp);
+//         int notRobbed = 0 + fun(ind-1, nums, dp);
         
-        return dp[ind] = max(robbed, notRobbed);
-    }
+//         return dp[ind] = max(robbed, notRobbed);
+//     }
 };
