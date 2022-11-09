e3f8e4d25310a6205213e36660276e1894d56edf
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-11-09 21:26:26 +0530

    Time: 3 ms (62.65%), Space: 8.1 MB (19.39%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -5,33 +5,54 @@ public:
         
         if(n == 1) return nums[0];
         
-        vector<int> dp1(n-1, -1);
-        vector<int> dp2(n, -1);
+        vector<int> dp1(n-1, 0);
+        vector<int> dp2(n, 0);
+        
+        // base-case
+        dp1[0] = nums[0];
+        dp2[1] = nums[1];
+        
+        // filling dp1
+        for(int ind = 1; ind < n-1; ++ind) {
+            int notPick = 0 + dp1[ind-1];
+            int pick = nums[ind];
+            if(ind > 1) pick += dp1[ind-2];
 
-        return max(fun1(n-2, nums, dp1), fun2(n-1, nums, dp2));
+            dp1[ind] = max(notPick, pick);
+        }
+        
+        // filling dp2
+        for(int ind = 2; ind < n; ++ind) {
+            int notPick = 0 + dp2[ind-1];
+            int pick = nums[ind] + dp2[ind-2];
+
+            dp2[ind] = max(notPick, pick);
+        }
+        
+        return max(dp1[n-2], dp2[n-1]);
     }
     
-    int fun1(int ind, vector<int> &nums, vector<int> &dp1) {
-        if(ind < 0) return 0;
-        if(ind == 0) return nums[0];
+//     int fun1(int ind, vector<int> &nums, vector<int> &dp1) {
+//         if(ind < 0) return 0;
+//         if(ind == 0) return nums[0];
         
-        if(dp1[ind] != -1) return dp1[ind];
+//         if(dp1[ind] != -1) return dp1[ind];
         
-        int notPick = 0 + fun1(ind-1, nums, dp1);
-        int pick = nums[ind] + fun1(ind-2, nums, dp1);
+//         int notPick = 0 + fun1(ind-1, nums, dp1);
+//         int pick = nums[ind] + fun1(ind-2, nums, dp1);
         
-        return dp1[ind] = max(notPick, pick);
-    }
+//         return dp1[ind] = max(notPick, pick);
+//     }
     
-    int fun2(int ind, vector<int> &nums, vector<int> &dp2) {
-        if(ind <= 0) return 0;
-        if(ind == 1) return nums[1];
+//     int fun2(int ind, vector<int> &nums, vector<int> &dp2) {
+//         if(ind <= 0) return 0;
+//         if(ind == 1) return nums[1];
         
-        if(dp2[ind] != -1) return dp2[ind];
+//         if(dp2[ind] != -1) return dp2[ind];
         
-        int notPick = 0 + fun2(ind-1, nums, dp2);
-        int pick = nums[ind] + fun2(ind-2, nums, dp2);
+//         int notPick = 0 + fun2(ind-1, nums, dp2);
+//         int pick = nums[ind] + fun2(ind-2, nums, dp2);
         
-        return dp2[ind] = max(notPick, pick);
-    }
+//         return dp2[ind] = max(notPick, pick);
+//     }
 };
