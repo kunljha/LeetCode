c095262143dbb4dec0195e0fd8975c4c556be29f
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-11-11 14:38:30 +0530

    Time: 418 ms (5.00%), Space: 67.8 MB (54.53%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,41 +3,26 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        // space-optimised-dp
+        vector<int> front(2, 0), cur(2, 0);
         
         // base-case
-        dp[n][0] = -1e9;
-        dp[n][1] = 0;
+        front[0] = -1e9;
+        front[1] = 0;
         
         for(int i = n-1; i >= 0; --i) {
-            int pick = nums[i] + dp[i+1][1];
+            int pick = nums[i] + front[1];
             int stop = 0; // means stop picking and return with cur sum
-            dp[i][1] = max(stop, pick);
+            cur[1] = max(stop, pick);
             
             
-            int notPick = 0 + dp[i+1][0];
-            pick = nums[i] + dp[i+1][1];
-            dp[i][0] = max(notPick, pick);      
+            int notPick = 0 + front[0];
+            pick = nums[i] + front[1];
+            cur[0] = max(notPick, pick); 
+            
+            front = cur;
         }
             
-        return dp[0][0];     
+        return front[0];     
     }
-    
-//     int fn(int i, int mustPick, vector<int> &nums, vector<vector<int>> &dp) {
-//         if(i == nums.size()) return mustPick ? 0 : -1e9;
-        
-//         if(dp[i][mustPick] != -1) return dp[i][mustPick];
-        
-//         if(mustPick) {
-//             int pick = nums[i] + fn(i+1, 1, nums, dp);
-//             int stop = 0; // means stop picking and return with cur sum
-            
-//             return dp[i][mustPick] = max(stop, pick);
-//         } else {
-//             int notPick = 0 + fn(i+1, 0, nums, dp);
-//             int pick = nums[i] + fn(i+1, 1, nums, dp);
-          
-//             return dp[i][mustPick] = max(notPick, pick);
-//         } 
-//     } 
 };
