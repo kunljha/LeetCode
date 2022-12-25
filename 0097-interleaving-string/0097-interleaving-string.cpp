e34b0666587f5d414e958685c04cba25c5cc6518
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2022-12-25 14:19:33 +0530

    Time: 59 ms (19.03%), Space: 79.8 MB (5.40%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -4,25 +4,26 @@ public:
         if(s1.size() + s2.size() != s3.size())
             return false;
         
-        vector<vector<int>> dp(s1.size()+1, vector<int>(s2.size()+1, -1));
+        vector<vector<vector<int>>> dp(s1.size()+1, vector<vector<int>>(s2.size()+1, vector<int>(s3.size()+1, -1)));
         
-        return fun(s1.size()-1, s2.size()-1, s3.size()-1, s1, s2, s3, dp);
+        return fun(0, 0, 0, s1, s2, s3, dp);
     }
     
-    bool fun(int i, int j, int k, string &s1, string &s2, string &s3, vector<vector<int>> &dp) {
-        if(i < 0 && j < 0 && k < 0) return 1;
+     bool fun(int i, int j, int k, string &s1, string &s2, string &s3, vector<vector<vector<int>>> &dp) {
+        if(i == s1.size() && j == s2.size() && k == s3.size()) return 1;
         
-        if(i >= 0 && j >= 0 && dp[i][j] != -1) return dp[i][j];
-          
-        if(i >= 0 && s1[i] == s3[k] && j >= 0 && s2[j] == s3[k])
-            return dp[i][j] = fun(i-1, j, k-1, s1, s2, s3, dp) || fun(i, j-1, k-1, s1, s2, s3, dp);
+        if(dp[i][j][k] != -1) return dp[i][j][k];
         
-        else if(i >= 0 && s1[i] == s3[k])
-            return fun(i-1, j, k-1, s1, s2, s3, dp);
         
-        else if(j >= 0 && s2[j] == s3[k])
-            return fun(i, j-1, k-1, s1, s2, s3, dp);
-        
-        else return 0;
+        if(i < s1.size() && s1[i] == s3[k] && j < s2.size() && s2[j] == s3[k]) {
+            return dp[i][j][k] = fun(i+1, j, k+1, s1, s2, s3, dp) || fun(i, j+1, k+1, s1, s2, s3, dp);
+        }
+        else if(i < s1.size() && s1[i] == s3[k]) {
+            return dp[i][j][k] = fun(i+1, j, k+1, s1, s2, s3, dp);
+        }
+        else if(j < s2.size() && s2[j] == s3[k]) {
+            return dp[i][j][k] = fun(i, j+1, k+1, s1, s2, s3, dp);
+        }
+        else return dp[i][j][k] = 0;
     }
 };
