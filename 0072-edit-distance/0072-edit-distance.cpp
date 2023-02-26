7d763978298f7fef31ad43391c67aca1ec2d69f4
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2023-02-26 23:48:19 +0530

    Time: 11 ms (77.86%), Space: 6.4 MB (93.87%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,22 +3,25 @@ public:
     int minDistance(string s1, string s2) {
         int n = s1.size(), m = s2.size();
         
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));  
+        vector<int> prev(m+1, 0), cur(m+1, 0);  
         
         // base-case
-        for(int j = 0; j <= m; ++j) dp[0][j] = j;
-        for(int i = 0; i <= n; ++i) dp[i][0] = i;
+        for(int j = 0; j <= m; ++j) prev[j] = j;
         //
         
         for(int i = 1; i <= n; ++i) {
+            cur[0] = i;
             for(int j = 1; j <= m; ++j) {
-                if(s1[i-1] == s2[j-1]) dp[i][j] = 0 + dp[i-1][j-1];
-        
-                else dp[i][j] = 1 + min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]));
+                if(s1[i-1] == s2[j-1])
+                    cur[j] = 0 + prev[j-1];
+                else
+                    cur[j] = 1 + min(prev[j], min(cur[j-1], prev[j-1]));
             }
+            
+            prev = cur;
         }
            
-        return dp[n][m];
+        return prev[m];
     }
     
 // ----------------- memoization -------------------
