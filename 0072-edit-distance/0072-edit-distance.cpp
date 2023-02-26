09f872a9f80d29ac3f2711c1a6e375a670dd37db
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2023-02-26 23:40:09 +0530

    Time: 12 ms (71.88%), Space: 9.1 MB (23.71%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,20 +3,35 @@ public:
     int minDistance(string s1, string s2) {
         int n = s1.size(), m = s2.size();
         
-        vector<vector<int>> dp(n, vector<int>(m, -1));  
-        return fun(n-1, m-1, s1, s2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));  
+        
+        // base-case
+        for(int j = 0; j <= m; ++j) dp[0][j] = j;
+        for(int i = 0; i <= n; ++i) dp[i][0] = i;
+        //
+        
+        for(int i = 1; i <= n; ++i) {
+            for(int j = 1; j <= m; ++j) {
+                if(s1[i-1] == s2[j-1]) dp[i][j] = 0 + dp[i-1][j-1];
+        
+                else dp[i][j] = 1 + min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]));
+            }
+        }
+           
+        return dp[n][m];
     }
     
-    int fun(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
-        if(i < 0) return j+1;
-        if(j < 0) return i+1;
+// ----------------- memoization -------------------
+//     int fun(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
+//         if(i < 0) return j+1;
+//         if(j < 0) return i+1;
         
-        if(dp[i][j] != -1) return dp[i][j];
+//         if(dp[i][j] != -1) return dp[i][j];
         
-        if(s1[i] == s2[j]) return dp[i][j] = 0 + fun(i-1, j-1, s1, s2, dp);
+//         if(s1[i] == s2[j]) return dp[i][j] = 0 + fun(i-1, j-1, s1, s2, dp);
         
-        return dp[i][j] = 1 + min(fun(i-1, j, s1, s2, dp),
-                       min(fun(i, j-1, s1, s2, dp), fun(i-1, j-1, s1, s2, dp)));
-    }
+//         return dp[i][j] = 1 + min(fun(i-1, j, s1, s2, dp),
+//                        min(fun(i, j-1, s1, s2, dp), fun(i-1, j-1, s1, s2, dp)));
+//     }
     
 };
