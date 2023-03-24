fa4789e6da1af08bf534583d7b31cf99d02ec14d
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2023-03-24 22:15:32 +0530

    Time: 548 ms (34.82%), Space: 170.2 MB (11.32%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,9 +1,11 @@
 class Solution {
+private: 
+    // we need (u, adj[u]) to store in ump 
+    unordered_map<int, vector<int>> adj, back;
 public:
-    // BFS
+    // DFS
     int minReorder(int n, vector<vector<int>>& connections) {
         // create adjacency list -- graph is directed
-        vector<vector<int>> adj(n), back(n);
         for(vector<int>& v : connections) {
             adj[v[0]].push_back(v[1]); // node a->b -- outward edge 
             back[v[1]].push_back(v[0]); // node a<-b -- inward edge
@@ -11,37 +13,33 @@ public:
         
         int res = 0;
         vector<bool> visited(n);
-        queue<int> q;
-        q.push(0);
         
-        while(not q.empty()) {
-            int u = q.front();
-            q.pop();
-            
-            visited[u] = true;
-            
-            for(int& v : adj[u]) {
-                if(!visited[v]) {
-                    // v-node is not visited and edge is facing outward from u to v,
-                    // we need to reorder this edge inward so increment res
-                    // pushing v-node in queue bcoz maybe its adjacent node's are 
-                    // also connected by outward edge to v
-                    ++res; 
-                    q.push(v);
-                }
+        dfs(0, visited, res);
+        
+        return res;   
+    }
+    
+    void dfs(int src, vector<bool> &visited, int &res) {
+        visited[src] = true;
+        
+        for(int& v : adj[src]) {
+            if(!visited[v]) {
+                ++res,
+                dfs(v, visited, res);
             }
-            
-            for(int& v : back[u]) {
-                if(!visited[v])
-                // v-node is not visited and edge is facing inward from u to v,
-                // so no need to reorder this already inward edge
-                // pushing v-node in queue bcoz maybe its adjacent node's are 
-                // connected by outward edge to v
-                q.push(v);    
-            }      
-            
         }
         
-        return res;     
+        for(int& v : back[src]) {
+            if(!visited[v])
+                dfs(v, visited, res);
+        }
     }
+    
 };
+
+
+
+
+
+
+
