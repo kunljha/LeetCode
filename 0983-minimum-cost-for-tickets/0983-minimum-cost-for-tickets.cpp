c0646da7996c325d47232f9e55f46156390ecb8d
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2023-03-28 14:46:57 +0530

    Time: 8 ms (30.37%), Space: 9.6 MB (57.33%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,13 +1,41 @@
 class Solution {
 public:
-    // Memoization
+    // Tabulation
     int mincostTickets(vector<int>& day, vector<int>& cost) {
         int n = day.size();
         
-        vector<int> dp(n+1, -1);
-        return fun(0, day, cost, dp);
+        vector<int> dp(n+1, 0);
+        
+        // base-case
+        dp[n] = 0;
+        // 
+        
+        for(int i = n-1; i >= 0; --i) {
+            // buy 1-day pass
+            int cost_one = cost[0] + dp[i+1];
+
+            // buy 7-day pass
+            int j = i+1;
+            for( ; j < n; ++j) {
+                if(day[j] >= day[i] + 7) break;
+            }
+            int cost_seven = cost[1] + dp[j];
+
+            // buy 30-day pass
+            int k = i+1;
+            for( ; k < n; ++k) {
+                if(day[k] >= day[i] + 30) break;
+            }
+            int cost_thirty = cost[2] + dp[k];
+
+            dp[i] = min(cost_one, min(cost_seven, cost_thirty)); 
+        }
+        
+        return dp[0];
     }
     
+    /*
+    // Memoization
     int fun(int i, vector<int> &day, vector<int> &cost, vector<int> &dp) {
         int n = day.size();
 
@@ -34,5 +62,6 @@ public:
 
         return dp[i] = min(cost_one, min(cost_seven, cost_thirty));   
     }
+    */
     
 };
