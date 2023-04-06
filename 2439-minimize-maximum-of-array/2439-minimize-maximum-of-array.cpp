8edec73cb913af74eaed776272bef6c5b010b493
Kunal Jha <76421345+kunljha@users.noreply.github.com>
2023-04-06 19:01:15 +0530

    Time: 130 ms (87.35%), Space: 71.5 MB (28.67%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    // idea : the best we can do is to reduce nums[i] to the average of array nums[0, i]
     int minimizeArrayValue(vector<int>& nums) {
         int n = nums.size();
-        long long res = 0, sum = 0;
         
+        long long sum = 0, res = 0;
         for(int i = 0; i < n; ++i) {
             sum += nums[i];
             
             long long cur_avg = ceil((double)sum / (i+1));
-            res = max(res, cur_avg);
+            res = max(res, cur_avg); 
         }
         
-        return res;
+        return res;   
     }
 };
